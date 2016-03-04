16a84aa3be1e836fab9f95ae15312557047be14f
dbdev138 <dbdev138@gmail.com>
2016-03-04 16:30:09 +0000

    DEV-4: Amended data type in model

--- build.sbt
+++ build.sbt
@@ -59,3 +59,5 @@ libraryDependencies ++= Seq(
 //fork in run := true
 
 fork in run := true
+
+fork in run := true
--- src/main/scala/com/models/Business.scala
+++ src/main/scala/com/models/Business.scala
@@ -4,18 +4,18 @@ trait Business
 
 object Business{
     
-    case class Business(id: Int, name: String, channel: String, channel_type: String, phone: Int, address: String, town: String, county: String, region: String )
+    case class Business(id: Int, name: String, channel: String, channel_type: String, phone: String, address: String, town: String, county: String, region: String )
     
     val businesses = List[Business](
-        Business(0, "Brownes Butchers", "food", "butchers", 18412113, "19 Bridge Street Balbriggan Co. Dublin", "Balbriggan","Dublin", "Leinster"),
-        Business(1, "Landmark Estates", "property", "estate agents", 18414726, "21 Drogheda Street Balbriggan Co. Dublin", "Balbriggan", "Dublin", "Leinster"),
-        Business(2, "Village Art Gallery", "art", "gallery", 18492236, "83 Strand Street Skerries Co. Dublin", "Skerries", "Dublin", "Leinster"),
-        Business(3, "Brownes Butchers", "food", "butchers", 18412113, "19 Bridge Street Balbriggan Co. Dublin", "Balbriggan","Dublin", "Leinster"),
-        Business(4, "Landmark Estates", "property", "estate agents", 18414726, "21 Drogheda Street Balbriggan Co. Dublin", "Balbriggan", "Dublin", "Leinster"),
-        Business(5, "Village Art Gallery", "art", "gallery", 18492236, "83 Strand Street Skerries Co. Dublin", "Skerries", "Dublin", "Leinster"),
-        Business(6, "Brownes Butchers", "food", "butchers", 18412113, "19 Bridge Street Balbriggan Co. Dublin", "Balbriggan","Dublin", "Leinster"),
-        Business(7, "Landmark Estates", "property", "estate agents", 18414726, "21 Drogheda Street Balbriggan Co. Dublin", "Balbriggan", "Wicklow", "Leinster"),
-        Business(8, "Village Art Gallery", "art", "gallery", 18492236, "83 Strand Street Skerries Co. Dublin", "Skerries", "Dublin", "Munster")
+        Business(0, "Brownes Butchers", "food", "butchers", "+35318412113", "19 Bridge Street Balbriggan Co. Dublin", "Balbriggan","Dublin", "Leinster"),
+        Business(1, "Landmark Estates", "property", "estate agents", "+35318414726", "21 Drogheda Street Balbriggan Co. Dublin", "Balbriggan", "Dublin", "Leinster"),
+        Business(2, "Village Art Gallery", "art", "gallery", "+35318492236", "83 Strand Street Skerries Co. Dublin", "Skerries", "Dublin", "Leinster"),
+        Business(3, "Brownes Butchers", "food", "butchers", "+35318412113", "19 Bridge Street Balbriggan Co. Dublin", "Balbriggan","Dublin", "Leinster"),
+        Business(4, "Landmark Estates", "property", "estate agents", "+35318414726", "21 Drogheda Street Balbriggan Co. Dublin", "Balbriggan", "Dublin", "Leinster"),
+        Business(5, "Village Art Gallery", "art", "gallery", "+35318492236", "83 Strand Street Skerries Co. Dublin", "Skerries", "Dublin", "Leinster"),
+        Business(6, "Brownes Butchers", "food", "butchers", "+35318412113", "19 Bridge Street Balbriggan Co. Dublin", "Balbriggan","Dublin", "Leinster"),
+        Business(7, "Landmark Estates", "property", "estate agents", "+35318414726", "21 Drogheda Street Balbriggan Co. Dublin", "Balbriggan", "Wicklow", "Leinster"),
+        Business(8, "Village Art Gallery", "art", "gallery", "+35318492236", "83 Strand Street Skerries Co. Dublin", "Skerries", "Dublin", "Munster")
       )
 
 }
